27dbc80d98baf6a89f9909a5cdc8fb6816d2bd35
onoffclass2 <62779283+onoffclass2@users.noreply.github.com>
2021-01-22 09:35:31 +0900

    create customs.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
+const customers=[{
+  'id':1,
+  'image' : 'http://placeimg.com/64/64/1',
+  'name':'유재석',
+  'birthday':'700102',
+  'gender' : '남자',
+  'job' : '연예인(런닝맨)'
+
+},
+{
+  'id':2,
+  'image' : 'http://placeimg.com/64/64/2',
+  'name':'하하',
+  'birthday':'790102',
+  'gender' : '남자',
+  'job' : '연예인(런닝맨)'
+
+},
+{
+  'id':3,
+  'image' : 'http://placeimg.com/64/64/3',
+  'name':'송지효',
+  'birthday':'810102',
+  'gender' : '여자',
+  'job' : '연예인(런닝맨)'
+
+}]
+/*
+class App extends Component{
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>어머나 src/App.jsasdf</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Customer
+        id={customers[0].id}
+        image={customers[0].image}
+        name={customers[0].name}
+        birthday={customers[0].birthday}
+        gender={customers[0].gender}
+        job={customers[0].job}
+        />
+        <Customer
+        id={customers[1].id}
+        image={customers[1].image}
+        name={customers[1].name}
+        birthday={customers[1].birthday}
+        gender={customers[1].gender}
+        job={customers[1].job}
+        />
+        <Customer
+        id={customers[2].id}
+        image={customers[2].image}
+        name={customers[2].name}
+        birthday={customers[2].birthday}
+        gender={customers[2].gender}
+        job={customers[2].job}
+        />
     </div>
   );
+}
+}
+*/
+class App extends Component{
+  render(){
+  return (
+    <div>{
+      customers.map(c => {
+        return(
+          <Customer  key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender}  job={c.job} />
+        );
+
+      })
+      }
+    </div>
+  );
+}
 }
 
 export default App;
---
+++ src/com/Customer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Customer extends React.Compontent {
+    render(){
+        return(
+            <div>
+                <h2>홍길동</h2>
+                <p>770902</p>
+                <p>남자 </p>
+                <p>대학생</p>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+
+export default  Customer;
+
